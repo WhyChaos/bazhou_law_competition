c33413b588c188a1aeae4dcdb5623d11e55ae84c
whynothc <nowadayshc@163.com>
2024-06-29 23:15:33 +0800

    sub company plan

--- tools/basic_function/search_company_name.py
+++ tools/basic_function/search_company_name.py
@@ -31,50 +31,67 @@ class SearchCompanyName(BaseFunction):
             data = []
         return data
 
-    def search_company_name_by_sub_wapper(self, key: str, value: str):
-        if key not in self.sub_key:
-            return f'不存在{key}这个字段名，请根据用户的内容中找出正确的字段名。'
-        data = []
-        data = self.search_company_name_by_sub(key, value)
-
-        if data == [] and (key == '关联上市公司股票简称' or key == '关联上市公司全称'):
-            data = self.search_company_name_by_sub('关联上市公司股票简称', value)
-            if data == []:
-                data = self.search_company_name_by_sub('关联上市公司全称', value)
-            if data == []:
-                tmp = self.search_company_name_by_info('公司简称', value)
-                if len(tmp) == 0:
-                    tmp = self.search_company_name_by_info('英文名称', value)
-                if len(tmp) > 0:
-                    data = self.search_company_name_by_sub('关联上市公司全称', tmp[0]['公司名称'])
-
-        if data == [] and key == '公司名称':
-            if data == []:
-                data = self.search_company_name_by_info('英文名称', value)
-            if data == []:
-                data = self.search_company_name_by_info('公司简称', value)
-
+    def get_sub_company_company(self, mother_company_name: str):
+        data = self.search_company_name_by_sub('关联上市公司全称', mother_company_name)
         if data == []:
-            return f'字段名或字段值提供错误，请根据用户的内容中找出正确的字段名和字段值。'
-        return data
-
-    def search_company_name_by_info_and_register(self, key: str, value: str):
-        if key not in self.info_key:
-            return f'不存在{key}这个字段名，请根据用户的内容中找出正确的字段名。'
-        data = []
-        if key in self.info_key:
-            data = self.search_company_name_by_info(key, value)
-            if data == []:
-                data = self.search_company_name_by_register(key, value)
-            if data == [] and (key == '公司名称' or key == '公司简称' or key == '英文名称'):
-                data = self.search_company_name_by_info('公司名称', value)
-                if data == []:
-                    data = self.search_company_name_by_info('英文名称', value)
-                if data == []:
-                    data = self.search_company_name_by_info('公司简称', value)
-
-
+            data = self.search_company_name_by_sub('关联上市公司股票简称', mother_company_name)
         if data == []:
-            return f'字段名或字段值提供错误，请根据用户的内容中找出正确的字段名和字段值。'
-        return data
+            tmp = self.search_company_name_by_info('公司简称', mother_company_name)
+            if len(tmp) == 0:
+                tmp = self.search_company_name_by_info('英文名称', mother_company_name)
+            if len(tmp) > 0:
+                data = self.search_company_name_by_sub('关联上市公司全称', tmp[0]['公司名称'])
+        if data != []:
+            return data
+
+        return '公司名称不存在，请根据用户输入提取出正确的公司名称'
+
+
+
+    # def search_company_name_by_sub_wapper(self, key: str, value: str):
+    #     if key not in self.sub_key:
+    #         return f'不存在{key}这个字段名，请根据用户的内容中找出正确的字段名。'
+    #     data = []
+    #     data = self.search_company_name_by_sub(key, value)
+    #
+    #     if data == [] and (key == '关联上市公司股票简称' or key == '关联上市公司全称'):
+    #         data = self.search_company_name_by_sub('关联上市公司股票简称', value)
+    #         if data == []:
+    #             data = self.search_company_name_by_sub('关联上市公司全称', value)
+    #         if data == []:
+    #             tmp = self.search_company_name_by_info('公司简称', value)
+    #             if len(tmp) == 0:
+    #                 tmp = self.search_company_name_by_info('英文名称', value)
+    #             if len(tmp) > 0:
+    #                 data = self.search_company_name_by_sub('关联上市公司全称', tmp[0]['公司名称'])
+    #
+    #     if data == [] and key == '公司名称':
+    #         if data == []:
+    #             data = self.search_company_name_by_info('英文名称', value)
+    #         if data == []:
+    #             data = self.search_company_name_by_info('公司简称', value)
+    #
+    #     if data == []:
+    #         return f'字段名或字段值提供错误，请根据用户的内容中找出正确的字段名和字段值。'
+    #     return data
+    #
+    # def search_company_name_by_info_and_register(self, key: str, value: str):
+    #     if key not in self.info_key:
+    #         return f'不存在{key}这个字段名，请根据用户的内容中找出正确的字段名。'
+    #     data = []
+    #     if key in self.info_key:
+    #         data = self.search_company_name_by_info(key, value)
+    #         if data == []:
+    #             data = self.search_company_name_by_register(key, value)
+    #         if data == [] and (key == '公司名称' or key == '公司简称' or key == '英文名称'):
+    #             data = self.search_company_name_by_info('公司名称', value)
+    #             if data == []:
+    #                 data = self.search_company_name_by_info('英文名称', value)
+    #             if data == []:
+    #                 data = self.search_company_name_by_info('公司简称', value)
+    #
+    #
+    #     if data == []:
+    #         return f'字段名或字段值提供错误，请根据用户的内容中找出正确的字段名和字段值。'
+    #     return data
 
---
+++ tools/basic_tool/get_sub_company_info_tool.py
@@ -0,0 +1,62 @@
+from tools.basic_function.get_company_info import GetCompanyInfo
+from tools.basic_function.search_company_name import SearchCompanyName
+
+
+class GetSubCompanyInfoTool:
+    def __init__(self):
+        self.tool_name = 'sub_company_info'
+        self.get_company_info = GetCompanyInfo()
+        self.search_company_name_by_info = SearchCompanyName()
+        self.name_to_display = {
+            '关联上市公司股票代码': '母公司股票代码',
+            '关联上市公司股票简称': '母公司股票简称',
+            '关联上市公司全称': '母公司全称',
+            '上市公司关系': '关系',
+            '上市公司参股比例': '参股比例',
+            '上市公司投资金额': '投资金额',
+            '公司名称': '子公司名称',
+        }
+
+    def get_glm_tool_dict(self) -> dict:
+        return {
+            "type": "function",
+            "function": {
+                "name": self.tool_name,
+                "description": "查子公司的工具：根据母公司名称获得总投资金额、所有子公司名称、数量和融资信息。",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "mother_company_name": {
+                            "description": "母公司名称",
+                            "type": "string"
+                        },
+                    },
+                    "required": ["mother_company_name"]
+                },
+            }
+        }
+
+    def run(self, mother_company_name: str):
+        info_list = []
+        sub_company_name_list = self.search_company_name_by_info.get_sub_company_company(mother_company_name=mother_company_name)
+        if isinstance(sub_company_name_list, str):
+            return sub_company_name_list
+        for sub_company_name in sub_company_name_list:
+            info_list.append(self.get_company_info.get_company_sub(company_name=sub_company_name['公司名称']))
+
+        info = {}
+        if info_list:
+            info['母公司全称'] = info_list[0]['关联上市公司全称']
+            info['母公司股票代码'] = info_list[0]['关联上市公司股票代码']
+            info['母公司股票简称'] = info_list[0]['关联上市公司股票简称']
+        info['子公司数量'] = len(info_list)
+        info['子公司信息'] = []
+        for item in info_list:
+            info['子公司信息'].append({
+                '子公司名称': item['公司名称'],
+                '关系': item['上市公司关系'],
+                '参股比例': item['上市公司参股比例'],
+                '投资金额': item['上市公司投资金额'],
+            })
+        return info
+
